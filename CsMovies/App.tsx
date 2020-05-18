d9bd0bde2c80a9e71be6b8f4217dbe8538b07be5
marco1mbella <mbellam@3il.fr>
2020-05-18 12:57:37 +0100

    test3

--- CsMovies/App.tsx
+++ CsMovies/App.tsx
@@ -5,12 +5,11 @@ import { Provider } from 'react-redux';
 import Store from './Store/ConfigureStore'
 import Navigation from './Navigation/Navigation';
 
-const salut="bonjour"
 export default function App() {
   return (
     <Provider store={Store}>
       <NavigationContainer>
-      {salut}   
+       <Navigation />    
       </NavigationContainer>
     </Provider>
   );
